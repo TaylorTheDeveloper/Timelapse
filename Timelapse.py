08b01371294e992755dea32c238b3ac265eeb413
taylor brockhoeft <taylorbrockhoeft@outlook.com>
2021-02-10 21:42:17 -0800

    init container and capture folder

--- Timelapse.py
+++ Timelapse.py
@@ -9,8 +9,15 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 scriptStartTime = datetime.datetime.utcnow()
 ###
 
+# Local Path for blobs to upload
+uploadsrcpath = "./capture/"
+
+# Get month from time
+time = datetime.datetime.now()
+month = time.strftime("%B")
 storageConnectionString = os.getenv('TimelapseAzureStorage')
 cameraName = os.environ.get('TimelapseCameraName')
+blobContainer = os.environ.get('TimelapseAzureBlobContainer')
 
 if not cameraName:
 	print("missing cameraName env variable")
@@ -20,7 +27,23 @@ if not storageConnectionString:
 	print("missing storageConnectionString env variable")
 	exit()
 
-print(cameraName)
+if not blobContainer:
+	container = "timelapse-"
+
+if not os.path.isdir(uploadsrcpath):
+	os.mkdir(uploadsrcpath)
+
+container = (blobContainer + month).lower()
+
+###
+
+try:
+	# Create the BlobServiceClient object
+	blob_service_client = BlobServiceClient.from_connection_string(storageConnectionString)
+
+except Exception as ex:
+	print("Failed to initate script")
+	print(ex)
 
 ###
 scriptEndTime = datetime.datetime.utcnow()
