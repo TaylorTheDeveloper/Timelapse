8b7974d619c965ac0b9ddab71e27fcd908e0461a
taylor <taylorthedeveloper@gmail.com@users.noreply.github.com>
2021-02-15 03:55:50 +0000

    add setup script

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Setting
+ssid="replace with your ssid"
+passphrase="replace with your passphrase"
+cameraName="timelapsecam"
+locale=en_US.UTF-8
+layout=us
+enableSSH=0
+enableCamera=0
+
+# Set keyboard layout and locale
+#raspi-config nonint do_change_locale $locale
+#raspi-config nonint do_configure_keyboard $layout
+
+# Enable SSH
+raspi-config nonint do_ssh $enableSSH
+
+# Enable camera
+raspi-config nonint do_camera $enableCamera
+
+# Set hostname from environment variable
+raspi-config nonint do_hostname $cameraName
+
+# Enable wifi country
+countryCode=US
+raspi-config nonint do_wifi_country $countryCode &> /dev/null
+
+# Set Wifi
+echo -en "\nnetwork={\n\tssid=\"$ssid\"\n\tpsk=\"$passphrase\"\n}" >> /etc/wpa_supplicant/wpa_supplicant.conf
+
+# Reboot
+sync
+reboot
