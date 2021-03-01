d2451cdd4e575dd9f9c29ded7598dfaf53df469e
taylor brockhoeft <taylorbrockhoeft@outlook.com>
2021-03-01 00:27:37 -0800

    adds cron job

--- installTimelapse.sh
+++ installTimelapse.sh
@@ -12,5 +12,6 @@ env >> /etc/environment
 # Setup cron job
 crontab -e root &> /dev/null
 (crontab -l ; echo "$TimelapseCameraFrequency /usr/bin/python3 /root/Timelapse/Timelapse.py > /root/out.txt") | sort - | uniq - | crontab -
+(crontab -l ; echo "$TimelapseCameraCloudUpdateFrequency /usr/bin/python3 /root/Timelapse/UpdateCheck.py > /root/updatecheck.txt") | sort - | uniq - | crontab -
 
 
