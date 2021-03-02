c1bf20cd8f185177fde7cda660121ab415516934
taylor brockhoeft <taylorbrockhoeft@outlook.com>
2021-03-02 01:17:34 -0800

    fix typo

--- installTimelapse.sh
+++ installTimelapse.sh
@@ -10,10 +10,10 @@ pip3 install azure-storage-blob
 env >> /etc/environment
 
 # Setup cron jobs
-crontab -l | grep -v '/usr/bin/python3 /root/Timelapse/'  | crontab -
+crontab -l | grep -v '/root/Timelapse/'  | crontab -
 crontab -e root &> /dev/null
 (crontab -l ; echo "$TimelapseCameraFrequency /usr/bin/python3 /root/Timelapse/Timelapse.py > /root/cameralog.txt") | sort - | uniq - | crontab -
 (crontab -l ; echo "$TimelapseCameraCloudUpdateFrequency /usr/bin/python3 /root/Timelapse/UpdateCheck.py > /root/updatechecklog.txt") | sort - | uniq - | crontab -
-(crontab -l ; echo "@reboot /root/Timelapse/installTimelapse.py > /root/bootlog.txt") | sort - | uniq - | crontab -
+(crontab -l ; echo "@reboot /root/Timelapse/installTimelapse.sh > /root/bootlog.txt") | sort - | uniq - | crontab -
 
 
